9f2f0c9af617f314669943a0ab91ce52bbe15181
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2023-12-06 23:17:33 -0300

    beforeEach, skip, todo, only , afterEach, before

--- 01-calltracker-vs-mock/index.test.js
+++ 01-calltracker-vs-mock/index.test.js
@@ -1,11 +1,33 @@
-import { describe, it, before } from 'node:test'
+import { describe, it, beforeEach, before, afterEach } from 'node:test'
+import { deepStrictEqual } from 'node:assert'
 
+function sum(a, b) {
+  return a + b
+}
 
 describe('Using Node.js only!!', () => {
-  before(() => {
-    // geralmente para limpar o estado
-    console.log('vou rodar antes de cada teste!')
+  // beforeEach(() => {
+  //   // geralmente para limpar o estado
+  //   console.log('vou rodar antes de cada teste!')
+  // })
+
+  // afterEach(() => {
+  //   console.log('vou rodar depois de cada teste!')
+  // })
+
+  // before(() => {
+  //   console.log('vou rodar antes de todos os testes!')
+  // })
+
+  it.todo('it should sum tow values', () => {
+    const expected = 10
+    const current = sum(5, 5)
+
+    deepStrictEqual(current, expected)
   })
 
-  it.todo('it should sum tow values')
+
+  it.skip('it should sum three values')
+
+  it('it should sum four values', { only: true })
 })
